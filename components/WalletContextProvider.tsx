0282122d7ecd8d85326f3e0820dfdd7a556ebf74
eyedocteur <edson.ichihara@gmail.com>
2022-09-11 15:31:38 -0300

    On branch main  Changes to be committed: 	modified:   components/WalletContextProvider.tsx

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react"
+import { FC, ReactNode  } from "react"
 import {
   ConnectionProvider,
   WalletProvider,
@@ -11,11 +11,11 @@ require("@solana/wallet-adapter-react-ui/styles.css")
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const url = useMemo(() => clusterApiUrl("devnet"), [])
-  const phantom = new PhantomWalletAdapter()
+  const phantom = useMemo(() => new PhantomWalletAdapter(), [])
 
   return (
     <ConnectionProvider endpoint={url}>
-      <WalletProvider wallets={[phantom]}>
+      <WalletProvider wallets={[phantom]} autoConnect={true}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
